f29542ffeaaf503e4dac01c1bcaa7e9b625aa185
Maki Hirose <Capture@Makis-MBP-RETINA-13.local>
2019-02-09 00:22:40 -0500

    190209 12:22pm Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+// TASKS
+
+    // HTML
+        // Create container for Instructions of Game.
+        // Create container for computer's score.
+        // Create 4 containers for Gem images.
+        // Create container for user total score.
+        // Create container for "You Win!" / "You Lose!" text to display.
+        // Create container for user's total wins.
+        // Create container for user's total losses.
+        // Create Play Next Game Button
+        // Create Reset Game button
+
+    // jQuery
+        // var randomNumberComputerScore - Random number generator for computer's score.
+
+        // var randomNumberGem1 - Random number generator for Gem 1.
+        // var numberAssignedToGem1 - Assign the random generated number for Gem 1 into a variable.
+
+        // var randomNumberGem2 - Random number generator for Gem 2.
+        // var numberAssignedToGem2 - Assign the random generated number for Gem 1 into a variable.
+
+        // var randomNumberGem3 - Random number generator for Gem 3.
+        // var numberAssignedToGem3 - Assign the random generated number for Gem 1 into a variable.
+
+        // var randomNumberGem4 - Random number generator for Gem 4.
+        // var numberAssignedToGem4 - Assign the random generated number for Gem 1 into a variable.
+
+        // var userInitialScore - User's initial score is set to 0
+        // var userTotalScore - When user presses a Gem, it's value gets added to the total score
+        
+        // If a user's total score is equal to the computer's score, "You Win!" is displayed in the HTML.
+        // New game begins and 1 is added to the user's total wins (Game cycles back to the start without reloading page).
+
+        // If a user's total score is greater than the computer's score, "You Lose!" is displayed in the HTML.
+        // New game begins and 1 is added to the user's total losses (Game cycles back to the start without reloading page).
+
+        // If user presses Reset Game button, page reloads.
+
+
+// ----------------------------------------
+
+// jQuery
+
+$(".gameInstructionsContainer").html("Game Instructions here");
+
+$(".")
+
+
